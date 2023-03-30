b3a655dd342298b1e391d12e593695c678afbff1
Zaydguendouze <z.guendouze@outlook.fr>
2023-03-30 19:38:21 +0200

    Mise en place des Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+
 import Home from "@/pages/Home";
 import Contact from "@/pages/Contact";
 import Service from "@/pages/Service";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./utils/Error";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Home />
-        <Service />
-        <Contact />
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+
+          <Route path="/home" element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
